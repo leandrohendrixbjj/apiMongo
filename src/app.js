03433115f685e540b0a2dd2fb05c4915a50b4b17
hendrix <leandrotully@gmail.com>
2022-08-26 21:51:14 -0300

    Evoluindo as rotas

--- src/app.js
+++ src/app.js
@@ -1,25 +1,9 @@
 import express from "express";
+import route from './route.js';
 
 const app = express();
 
 app.use(express.json());
-
-const motos = [
-    {id:'1',nome:"Vstrom 660"},
-    {id:'2',nome:"Honda CB 500x"}
-];
-
-app.get('/', (req,res) => {
-    res.status(200).send('Welcome to nodeJs');
-});
-
-app.get('/motos', (req,res) => {
-    res.status(200).json(motos)
-});
-
-app.post('/motos', (req,res) => {
-    motos.push(req.body);
-    res.status(201).end();
-});
+app.use('/api/livros', route);
 
 export default app;
---
+++ src/route.js
@@ -0,0 +1,56 @@
+import express from 'express';
+const route = express.Router();
+
+const livros = [
+   {id:'1',nome:"O mundo de Sofia"},
+   {id:'2',nome:"Cronicas de Gelo e Fogo"}
+];
+
+route.post('/', (req,res) => {
+    livros.push(req.body);
+    res.status(201).end();
+});
+
+route.get('/', (req,res) => {
+    res.status(200).json(livros);
+});
+
+route.get('/:id', buscaLivro(livros), (req,res) => {
+   const livroIndex = res.buscaLivro;
+   try{
+      if (livroIndex == -1)
+        throw Error;
+
+      res.json(livros[livroIndex]).end();  
+   }catch(err){
+      res.status(404);
+      res.json({error:'Livro não encontrado'});
+   }
+});
+
+route.put('/:id', buscaLivro(livros), (req,res) => {   
+    try{
+      const indexLivro = res.buscaLivro;
+
+      if (indexLivro == -1)
+         throw Error;
+
+      let livro = livros[indexLivro];
+      livro.nome = req.body.nome;
+
+      res.json(livro).end();  
+    }catch(err){
+       res.status(400);
+       res.json({message:'Livro não encontrado'});
+    }
+});
+
+function buscaLivro(livros){
+   return (req,res,next) => {
+      const livro = livros.findIndex(livro => livro.id == req.params.id);
+      res.buscaLivro = livro;
+      next();         
+   }
+}
+
+export default route;

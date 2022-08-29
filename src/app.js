7bacb5d535821d2f8d01a93e84d57dac3476f968
hendrix <leandrotully@gmail.com>
2022-08-29 07:22:54 -0300

    Iniciando com Mongoose

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import route from './route.js';
+import db from './config/connection.js';
+
+db.on("error", console.log.bind(console,'Error on database connection'));
+db.once("open", () => {
+    console.log('Connection was made successfully');
+});
+
 
 const app = express();
-
 app.use(express.json());
 app.use('/api/livros', route);
 
